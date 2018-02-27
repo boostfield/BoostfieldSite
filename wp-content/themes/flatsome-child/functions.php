ea425f72fbe5298cffbfa73e76779e78d8367c2d
qky <qky1412@gmail.com>
2018-02-27 15:28:53 +0800

    去掉了后台管理界面左上角的wordpress标志

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,2 +1,8 @@
 <?php
-// Add custom Theme Functions here
+function annointed_admin_bar_remove() {
+    global $wp_admin_bar;
+    /* Remove their stuff */
+    $wp_admin_bar->remove_menu('wp-logo');
+}
+add_action('wp_before_admin_bar_render', 'annointed_admin_bar_remove', 0);
+?>
