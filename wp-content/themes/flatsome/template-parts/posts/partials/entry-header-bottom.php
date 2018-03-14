217b7028f897cec3bb94db56b8e57616db855897
qky <qky1412@gmail.com>
2018-03-14 16:46:51 +0800

    reset entry-header-bottom

--- wp-content/themes/flatsome/template-parts/posts/partials/entry-header-bottom.php
+++ wp-content/themes/flatsome/template-parts/posts/partials/entry-header-bottom.php
@@ -5,14 +5,8 @@
 		   	<?php get_template_part( 'template-parts/posts/partials/entry', 'post-date'); ?>
 		</div><!-- .entry-image -->
 	<?php } ?>
-	<?php if ( in_category('case') ): ?>
-		<div class="category-case entry-header-text entry-header-text-bottom text-<?php echo flatsome_option('blog_posts_title_align');?>">
-			<?php get_template_part( 'template-parts/posts/partials/entry', 'title'); ?>
-		</div>
-	<?php ?>
-	<?php else: ?>
-		<div class="entry-header-text entry-header-text-bottom text-<?php echo flatsome_option('blog_posts_title_align');?>">
-			<?php get_template_part( 'template-parts/posts/partials/entry', 'title'); ?>
-		</div>
-	<?php endif;?>
+
+	<div class="entry-header-text entry-header-text-bottom text-<?php echo flatsome_option('blog_posts_title_align');?>">
+	   	<?php get_template_part( 'template-parts/posts/partials/entry', 'title'); ?>
+	</div>
 </header><!-- .entry-header -->
