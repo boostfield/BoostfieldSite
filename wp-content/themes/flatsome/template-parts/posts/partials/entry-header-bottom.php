b4793e07da43e96866bf4e164427c7dd976a9125
qky <qky1412@gmail.com>
2018-03-14 16:41:34 +0800

    增加了案例单页文章头部信息的区分

--- wp-content/themes/flatsome/template-parts/posts/partials/entry-header-bottom.php
+++ wp-content/themes/flatsome/template-parts/posts/partials/entry-header-bottom.php
@@ -5,8 +5,14 @@
 		   	<?php get_template_part( 'template-parts/posts/partials/entry', 'post-date'); ?>
 		</div><!-- .entry-image -->
 	<?php } ?>
-
-	<div class="entry-header-text entry-header-text-bottom text-<?php echo flatsome_option('blog_posts_title_align');?>">
-	   	<?php get_template_part( 'template-parts/posts/partials/entry', 'title'); ?>
-	</div>
+	<?php if ( in_category('case') ): ?>
+		<div class="category-case entry-header-text entry-header-text-bottom text-<?php echo flatsome_option('blog_posts_title_align');?>">
+			<?php get_template_part( 'template-parts/posts/partials/entry', 'title'); ?>
+		</div>
+	<?php ?>
+	<?php else: ?>
+		<div class="entry-header-text entry-header-text-bottom text-<?php echo flatsome_option('blog_posts_title_align');?>">
+			<?php get_template_part( 'template-parts/posts/partials/entry', 'title'); ?>
+		</div>
+	<?php endif;?>
 </header><!-- .entry-header -->
